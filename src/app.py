f695d1789b181a8cc569c6cf2c312ffb780f1379
AndreuJove <andreujove@gmail.com>
2026-02-14 15:12:19 +0100

    Refactor app.py

--- src/app.py
+++ src/app.py
@@ -1,9 +1,7 @@
 from flask import Flask
-import os
 from src.hello.routes import hello_routes
 
 def create_app():
     app = Flask(__name__)
-    os.makedirs(app.instance_path, exist_ok=True)
     app.register_blueprint(hello_routes)
     return app
