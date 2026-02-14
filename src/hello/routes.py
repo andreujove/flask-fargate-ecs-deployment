7195ca0d6c4b6aeabccb0b702cfad2d26ca7ea5f
AndreuJove <andreujove@gmail.com>
2026-02-14 14:14:27 +0100

    Refactor tests and routes

--- src/hello/routes.py
+++ src/hello/routes.py
@@ -2,6 +2,14 @@ from flask import Blueprint
 
 hello_routes = Blueprint('hello_routes', __name__)
 
+@hello_routes.route("/", methods=['GET'])
+def index():
+    return {
+        "message": "Success",
+        "status": "online"
+    }, 200
+
 @hello_routes.route("/hello", methods=['GET'])
 def hello():
-    return "Hello, World"
+    return "Hello, World", 200
+
--- tests/test_hello.py
+++ tests/test_hello.py
@@ -1,9 +1,14 @@
 
-def test_hello_page(client):
+def test_index_is_success(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.json.get('status') == 'online'
+
+def test_hello_page_is_success(client):
     response = client.get('/hello')
     assert response.status_code == 200
-    assert response.data == b'Hello, World'
+    assert response.get_data(as_text=True) == 'Hello, World'
 
 def test_not_found(client):
-    response = client.get('/notfound')
+    response = client.get('/does-not-exist')
     assert response.status_code == 404
